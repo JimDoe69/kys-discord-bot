9dbfa2fab5037c2ffa73e515d5f152dbe9d6aadf
JimDoe69 <33529187+JimDoe69@users.noreply.github.com>
2017-12-02 21:48:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const bot = new Discord.Client();
 
 bot.on('guildMemberAdd', function(member) {
-    member.guild.channels.find('name', 'general').sendMessage(member.toString() + ' Welcome To KYSGaming's Discord, You Are Now A Member!!');
+    member.guild.channels.find('name', 'general').sendMessage(member.toString() + ' Welcome To Our Discord, You Are Now A Member!!');
 
     member.addRole(member.guild.roles.find('name', 'Member'));
 });
@@ -13,4 +13,4 @@ bot.on('guildMemberAdd', function(member) {
 
                           
 
-bot.login('Mzg2NTQ3OTY0NDE3NDA5MDI0.DQT0TA.mPtBKZ8mudLba_BxbeoUUBAIlHc');
+bot.login('Mzg2NTQ3OTY0NDE3NDA5MDI0.DQT3Rw.t9qFv08Q3TyHYjjqaMkKoNN1DgY');
