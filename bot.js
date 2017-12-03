656a4460297a61f3abc8b90b15cbed3fe13ea050
JimDoe69 <33529187+JimDoe69@users.noreply.github.com>
2017-12-03 11:00:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
- 
 bot.on('guildMemberAdd', function(member) {
        member.guild.channels.find('name', 'general').sendMessage(member.toString() + ' Welcome To Our Discord, You Are Now A Member!!');
-       member.addRole(member.guild.roles.find('name', 'Member'));
-    }   
+       member.addRole(member.guild.roles.find('name', 'Member'))   
 });
+
+ 
 
 
 // THIS  MUST  BE  THIS  WAY
