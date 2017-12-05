b0c036456e3e5ffabb7152712227a10899103765
JimDoe69 <33529187+JimDoe69@users.noreply.github.com>
2017-12-05 13:37:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const bot = new Discord.Client();
 
 bot.on('guildMemberAdd', function(member) {
        member.guild.channels.find('name', 'general').sendMessage(member.toString() + ' Welcome To KYSGaming, Have Fun!!');
+      
        member.addRole(member.guild.roles.find('name', 'Member'))   
 });
 
